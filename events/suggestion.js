c2a23b5a7a7edef5ff4810d04c8684f657a3e01c
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-01-12 13:31:15 +0200

    Update suggestion.js

--- events/suggestion.js
+++ events/suggestion.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 module.export = (message, client) => {
+                if (message.channel.name !== '・𝚂uggestions') return;
 		const sugembed = new MessageEmbed()
 			.setColor('#6281F0')
 			.setAuthor('Vagmemes Suggestions', message.guild.iconURL())
