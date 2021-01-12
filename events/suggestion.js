ef9ef3cf6daba9f77744ea1c98a1f035cb99e407
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-01-12 13:01:04 +0200

    Add files via upload

---
+++ events/suggestion.js
@@ -0,0 +1,17 @@
+module.export = {
+  name: 'suggestion',
+  execute(message, client){
+		const sugembed = new Discord.MessageEmbed()
+			.setColor('#6281F0')
+			.setAuthor('Vagmemes Suggestions', message.guild.iconURL())
+			.setDescription(`**📑 \`Suggestion\` » \n ${message.content}**\n\n`)
+			.setThumbnail(message.author.avatarURL({ dynamic: true }))
+			.setFooter(`By » ${message.author.username}`)
+			.setTimestamp();
+		message.channel.send(sugembed).then(sug => {
+		await message.delete();
+		await sug.react('796800628927234078');
+		await sug.react('796800690428182558');
+		}).catch(() => message.channel.send('**:no_entry: - An error occurred while sending the suggestion, please try again later')
+  }
+}
