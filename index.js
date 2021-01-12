2e6b150e95d2b22843d1fb6bfea1b573c9c6f06a
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-01-12 13:51:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,20 @@ client.once('disconnect', () => {
 	console.log('Disconnect!');
 });
 client.on('message', async (message) => {
-        require('./events/suggestion')(message);
+                if (message.author.bot) return;
+                if (message.channel.name !== '・𝚂uggestions') return;
+		const sugembed = new MessageEmbed()
+			.setColor('#6281F0')
+			.setAuthor('Vagmemes Suggestions', message.guild.iconURL())
+			.setDescription(`**📑 \`Suggestion\` » \n ${message.content}**\n\n`)
+			.setThumbnail(message.author.avatarURL({ dynamic: true }))
+			.setFooter(`By » ${message.author.username}`)
+			.setTimestamp();
+		message.channel.send(sugembed).then(async sug => {
+		await message.delete();
+		await sug.react('796800628927234078');
+		await sug.react('796800690428182558');
+		}).catch(() => message.channel.send('**:no_entry: - An error occurred while sending the suggestion, please try again later'));
 });
 client.on('message', async (message) => {
 	const args = message.content.slice(prefix.length).split(/ +/);
